59d508f5cace073a3504f4c450851fff271a4fb1
mahfooz <mahfooz720946@gmail.com>
2024-10-19 17:50:18 +0530

    feat: define routes
    Signed-off-by: mahfooz <mahfooz720946@gmail.com>

--- backend/routes/route.go
+++ backend/routes/route.go
@@ -1,29 +1,14 @@
-// this file handles routing of the user request to respective handlers
+// Package routes : this file handles routing of the user request to respective handlers
 package routes
 
 import (
-	"database/sql"
 	"github.com/IamMahfhooz/fampay-assignment/handlers"
-	"fmt"
 	"github.com/labstack/echo/v4"
-	"log"
 	"net/http"
 )
 
-func AssignHandlers(db *sql.DB, e *echo.Echo, env map[string]string) {
-	// Verify connection
-	err := db.Ping()
-	if err != nil {
-		fmt.Println("from routes")
-		log.Fatal(err)
-	}
-	fmt.Println("Successfully connected to the database from handlers!")
-	var keywords []string
-	h := &handlers.DbHandler{
-		Keywords: keywords,
-		DB:       db,
-		Env:      env,
-	}
+func AssignHandlers(e *echo.Echo, h *handlers.DbHandler) {
+
 	e.POST("/youtube", h.FetchYoutube)
 	e.POST("/database", h.FetchDatabase)
 	e.GET("/ping", func(c echo.Context) error { return c.JSON(http.StatusOK, "i'm alive") })
