5315b4832f803cfb91f7b23b956b04c1c81d82dc
mahfooz <mahfooz720946@gmail.com>
2024-10-19 23:30:49 +0530

    feat: add feature to find best matching keyword from database
    Signed-off-by: mahfooz <mahfooz720946@gmail.com>

---
+++ backend/utils/aiSearch.go
@@ -0,0 +1,99 @@
+// Package utils : this file handles the promt modifying feature to query from database/youtube for particular keyword
+package utils
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/google/generative-ai-go/genai"
+	"google.golang.org/api/option"
+)
+
+type Content struct {
+	Parts []string `json:"Parts"`
+	Role  string   `json:"Role"`
+}
+type Candidates struct {
+	Content *Content `json:"Content"`
+}
+type ContentResponse struct {
+	Candidates *[]Candidates `json:"Candidates"`
+}
+
+func PrettyPrompt(keyword string, gemini_api_key string) (string, error) {
+	//initializing the gemini
+	ctx := context.Background()
+	client, err := genai.NewClient(ctx, option.WithAPIKey(gemini_api_key))
+	if err != nil {
+		return keyword, err
+	}
+	defer func(client *genai.Client) {
+		err := client.Close()
+		if err != nil {
+
+		}
+	}(client)
+	model := client.GenerativeModel("gemini-1.5-flash")
+	prompt := []genai.Part{
+		genai.Text("original promt = " + keyword + " ; Instructions for response :  1. Return the modified promt that gives most related youtube videos though youtube video search api ; If you think the user is searching for particular keyword  then return the original promt itself 2. Return only the modified promt without any other texts ( not even double inverted commas)"),
+	}
+	resp, err := model.GenerateContent(ctx, prompt...)
+	if err != nil {
+		return keyword, err
+	}
+	marshalResponse, _ := json.MarshalIndent(resp, "", "  ")
+	var generateResponse ContentResponse
+	if err := json.Unmarshal(marshalResponse, &generateResponse); err != nil {
+		return keyword, err
+	}
+	for _, cad := range *generateResponse.Candidates {
+		if cad.Content != nil {
+			for _, part := range cad.Content.Parts {
+				//fmt.Print(part)
+				return part, nil
+			}
+		}
+	}
+	return keyword, nil
+}
+func ReverseSearchDB(h *DbHandler, keyword string, gemini_api_key string) (string, error) {
+	query := "{ "
+	for key, _ := range h.Keywords {
+		query += key + " } , { "
+	}
+
+	ctx := context.Background()
+	client, err := genai.NewClient(ctx, option.WithAPIKey(gemini_api_key))
+	if err != nil {
+		return keyword, err
+	}
+	defer func(client *genai.Client) {
+		err := client.Close()
+		if err != nil {
+			fmt.Println("Unable to close gemini client")
+		}
+	}(client)
+
+	model := client.GenerativeModel("gemini-1.5-flash")
+	prompt := []genai.Part{
+		genai.Text("query = " + keyword + " array of strings = " + query + " } . You are required to output the best matching string for given query . If found , just return only that string else return \"NO\" . Just output only the string or NO ; nothing else and output without double inverted commas ; no new lines also at end ; just the string only"),
+	}
+	resp, err := model.GenerateContent(ctx, prompt...)
+	if err != nil {
+		return keyword, err
+	}
+	marshalResponse, _ := json.MarshalIndent(resp, "", "  ")
+	var generateResponse ContentResponse
+	if err := json.Unmarshal(marshalResponse, &generateResponse); err != nil {
+		return keyword, err
+	}
+	for _, cad := range *generateResponse.Candidates {
+		if cad.Content != nil {
+			for _, part := range cad.Content.Parts {
+				//fmt.Print(part)
+				return part, nil
+			}
+		}
+	}
+	return "", nil
+}
