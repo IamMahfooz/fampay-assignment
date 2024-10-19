bddc9c2a326a2a2865182c04361e869ab2173d81
mahfooz <mahfooz720946@gmail.com>
2024-10-19 21:46:34 +0530

    feat: resolve cycle import error
    Signed-off-by: mahfooz <mahfooz720946@gmail.com>

--- backend/utils/cronJob.go
+++ backend/utils/cronJob.go
@@ -1,28 +1,29 @@
 // Package utils : this file handles concurrent fetching of videos from youtube and storing in the database
 package utils
 
-import (
-	"fmt"
-	"github.com/IamMahfhooz/fampay-assignment/handlers"
-	"time"
-)
-
-func cronjob(h *DbHandler) {
-	for {
-		for _, key := range h.Keywords {
-			req := &handlers.YoutubeRequest{
-				Modify:        false,
-				Keyword:       key,
-				MaxResults:    10,
-				NextPageToken: "",
-				StartFrom:     10,
-			}
-			_, err := handlers.RunSearchQuery((*handlers.DbHandler)(h), req, key)
-			if err != nil {
-				fmt.Println("unable to perform cronjob")
-			}
-		}
-		time.Sleep(30 * time.Minute)
-	}
-
-}
+//
+//import (
+//	"fmt"
+//	"github.com/IamMahfhooz/fampay-assignment/handlers"
+//	"time"
+//)
+//
+//func cronjob(h *DbHandler) {
+//	for {
+//		for _, key := range h.Keywords {
+//			req := &handlers.YoutubeRequest{
+//				Modify:        false,
+//				Keyword:       key,
+//				MaxResults:    10,
+//				NextPageToken: "",
+//				StartFrom:     10,
+//			}
+//			_, err := handlers.RunSearchQuery((*handlers.DbHandler)(h), req, key)
+//			if err != nil {
+//				fmt.Println("unable to perform cronjob")
+//			}
+//		}
+//		time.Sleep(30 * time.Minute)
+//	}
+//
+//}
