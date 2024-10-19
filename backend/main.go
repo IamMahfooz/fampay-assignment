e137bbd0ef505082b784767a52b5ffa4f0fdbf34
mahfooz <mahfooz720946@gmail.com>
2024-10-19 15:36:23 +0530

    feat: code to start main server for client request
    Signed-off-by: mahfooz <mahfooz720946@gmail.com>

--- backend/main.go
+++ backend/main.go
@@ -1,1 +1,36 @@
 package main
+import (
+	"github.com/IamMahfhooz/fampay-assignment/routes"
+	"fmt"
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	_ "github.com/lib/pq"
+	"os"
+)
+
+func main() {
+	envFile, _ := godotenv.Read("../.env")
+	fmt.Println("loaded env variables")
+
+	e := echo.New()
+	e.Use(middleware.CORS())
+
+	db := routes.StartDbProcess(envFile, e)
+	defer db.Close()
+
+	routes.AssignHandlers(db, e, envFile)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = ":5004"
+	} else {
+		port = ":" + port
+	}
+
+	err := e.Start(port)
+	if err != nil {
+		fmt.Println("error while starting server : ", err.Error())
+		return
+	}
+}
