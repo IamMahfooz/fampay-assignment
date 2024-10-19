86c8ab16a75e1f3af6d4dd605c7ce523fccdb209
mahfooz <mahfooz720946@gmail.com>
2024-10-19 18:09:34 +0530

    feat: add basic cronjob feature
    Signed-off-by: mahfooz <mahfooz720946@gmail.com>

--- backend/utils/cronJob.go
+++ backend/utils/cronJob.go
@@ -1,2 +1,28 @@
-// this file handles concurrent fetching of videos from youtube and storing in the database
+// Package utils : this file handles concurrent fetching of videos from youtube and storing in the database
 package utils
+
+import (
+	"fmt"
+	"github.com/IamMahfhooz/fampay-assignment/handlers"
+	"time"
+)
+
+func cronjob(h *DbHandler) {
+	for {
+		for _, key := range h.Keywords {
+			req := &handlers.YoutubeRequest{
+				Modify:        false,
+				Keyword:       key,
+				MaxResults:    10,
+				NextPageToken: "",
+				StartFrom:     10,
+			}
+			_, err := handlers.RunSearchQuery((*handlers.DbHandler)(h), req, key)
+			if err != nil {
+				fmt.Println("unable to perform cronjob")
+			}
+		}
+		time.Sleep(30 * time.Minute)
+	}
+
+}
